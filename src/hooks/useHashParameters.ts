f0a46171ad13378d7b092f9d1ca0a00297b8f23a
Jakob Heuser <jakob@codedrift.com>
2025-10-05 21:34:23 -0700

    feat: Adds infra for shadCN support

--- src/hooks/useHashParameters.ts
+++ src/hooks/useHashParameters.ts
@@ -1,24 +1,33 @@
+import type { DeepPartial } from "node_modules/astro/dist/type-utils";
 import { useEffect, useState } from "react";
 
-export const useHashParameters = () => {
+export const useHashParameters = <T>(defaults?: DeepPartial<T>) => {
   // watches the hash parameters for changes and returns a memoized object of the parameters
   const [hashParams, setHashParams] = useState(() => {
     const params = new URLSearchParams(window.location.hash.slice(1));
-    const obj: Record<string, string> = {};
+    const obj: Record<string, string> = {
+      ...((defaults as Record<string, string>) ?? {}),
+    };
     params.forEach((value, key) => {
-      obj[key] = value;
+      if (value && value.length > 0) {
+        obj[key] = value;
+      }
     });
-    return obj;
+    return obj as DeepPartial<T>;
   });
 
   useEffect(() => {
     const onHashChange = () => {
       const params = new URLSearchParams(window.location.hash.slice(1));
-      const obj: Record<string, string> = {};
+      const obj: Record<string, string> = {
+        ...((defaults as Record<string, string>) ?? {}),
+      };
       params.forEach((value, key) => {
-        obj[key] = value;
+        if (value && value.length > 0) {
+          obj[key] = value;
+        }
       });
-      setHashParams(obj);
+      setHashParams(obj as DeepPartial<T>);
     };
 
     window.addEventListener("hashchange", onHashChange);
