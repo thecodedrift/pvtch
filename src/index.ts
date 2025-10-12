44b27d8197bcc582183bd0e17cdff232b3e43573
Jakob Heuser <jakob@codedrift.com>
2025-10-12 15:18:34 -0700

    fix: Adds blockConcurrencyWhile to ensure DO handles one input at a time

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ const TTL = 86400; // 1 day
  * A Durable Object's behavior is defined in an exported Javascript class
  * PvtchBackend is designed to provide a single input gate for mutations of data. It's effectively
  * an in memory queue without creating an entire queue system.
+ *
+ * The Durable Object uses blockConcurrencyWhile to ensure that only one request
+ * is being processed at a time. This ensures that write operations are
+ * in serial while reads can be done in parallel off the KV store.
  */
 export class PvtchBackend extends DurableObject<Env> {
   /**
@@ -21,38 +25,42 @@ export class PvtchBackend extends DurableObject<Env> {
     super(ctx, env);
   }
 
-  /**
-   * The Durable Object exposes an RPC method sayHello which will be invoked when a Durable
-   *  Object instance receives a request from a Worker via the same method invocation on the stub
-   *
-   * @returns The greeting to be sent back to the Worker
-   */
   async increment(token: string, value: number): Promise<number> {
-    // inside the DO, these are atomic operation
-    // let next = (await this.ctx.storage.get<number>(token)) || 0;
-    let fromKv = (await this.env.PVTCH_KV.get(token)) ?? "";
-    if (fromKv.length === 0) {
-      fromKv = "0";
-    }
+    let next = 0;
 
-    const next = Number.parseInt(fromKv, 10) + value;
+    // use blockConcurrencyWhile to specifically ensure the DO processes one request
+    // at a time for its identifier
+    // required because PVTCH_KV is eventually consistent and we want to ensure
+    // updates arrive in order. If this becomes an issue, we can migrate to a proper queue
+    this.ctx.blockConcurrencyWhile(async () => {
+      // inside the DO, these are atomic operation
+      // let next = (await this.ctx.storage.get<number>(token)) || 0;
+      let fromKv = (await this.env.PVTCH_KV.get(token)) ?? "";
+      if (fromKv.length === 0) {
+        fromKv = "0";
+      }
 
-    // You do not have to worry about a concurrent request having modified the value in storage.
-    // "input gates" will automatically protect against unwanted concurrency.
-    // Read-modify-write is safe.
-    // await this.ctx.storage.put(token, next);
-    await this.env.PVTCH_KV.put(token, next.toString(), {
-      // expires in 24 hours
-      expirationTtl: TTL,
+      next = Number.parseInt(fromKv, 10) + value;
+
+      // You do not have to worry about a concurrent request having modified the value in storage.
+      // "input gates" will automatically protect against unwanted concurrency.
+      // Read-modify-write is safe.
+      // await this.ctx.storage.put(token, next);
+      await this.env.PVTCH_KV.put(token, next.toString(), {
+        // expires in 24 hours
+        expirationTtl: TTL,
+      });
     });
 
     return next;
   }
 
   async set(token: string, value: number | string): Promise<number | string> {
-    await this.env.PVTCH_KV.put(token, value.toString(), {
-      // expires in 24 hours
-      expirationTtl: TTL,
+    this.ctx.blockConcurrencyWhile(async () => {
+      await this.env.PVTCH_KV.put(token, value.toString(), {
+        // expires in 24 hours
+        expirationTtl: TTL,
+      });
     });
 
     return value;
