7b14b167c1872ac234b99e09b1252a190a43cd18
Jakob Heuser <jakob@codedrift.com>
2025-10-20 21:28:57 -0700

    wip: Tries a more friendly CSP version of the embed

--- src/pages/pip/_components/render.tsx
+++ src/pages/pip/_components/render.tsx
@@ -15,27 +15,15 @@ export const Render: React.FC = () => {
   const params = useHashParameters<Options>({});
   const user = (params.user ?? defaults.user).replace(/[^a-z0-9_]/gi, "");
 
-  useEffect(() => {
-    if (!(globalThis as GlobalThisWithTwitch).Twitch) {
-      console.error("Twitch embed script not loaded");
-      return;
-    }
+  if (!user || user.length === 0) {
+    return null;
+  }
 
-    const options = {
-      width: 1920,
-      height: 1080,
-      channel: user,
-      // only needed if your site is also embedded on embed.example.com and othersite.example.com
-      parent: ["pvtch.com", "www.pvtch.com", "localhost:4322"],
-      autoplay: true,
-      muted: true,
-    };
-
-    const player = new (globalThis as GlobalThisWithTwitch).Twitch.Player(
-      "twitch-embed",
-      options,
-    );
-  }, []);
-
-  return <div id="twitch-embed"></div>;
+  return (
+    <iframe
+      src="https://player.twitch.tv/?channel=theCodeDrift&parent=www.pvtch.com&muted=true&autoplay=true"
+      width="1920"
+      height="1080"
+    ></iframe>
+  );
 };
