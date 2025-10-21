8bfc5f4105c1c3afa3b7724647c4b7077980bdc5
Jakob Heuser <jakob@codedrift.com>
2025-10-20 21:33:04 -0700

    wip: Adds configurable pip support as embed

--- src/pages/pip/_components/render.tsx
+++ src/pages/pip/_components/render.tsx
@@ -19,9 +19,10 @@ export const Render: React.FC = () => {
     return null;
   }
 
+  // https://dev.twitch.tv/docs/embed/video-and-clips/#non-interactive-inline-frames-for-live-streams-and-vods
   return (
     <iframe
-      src="https://player.twitch.tv/?channel=theCodeDrift&parent=www.pvtch.com&muted=true&autoplay=true"
+      src={`https://player.twitch.tv/?channel=${user}&parent=www.pvtch.com&muted=true&autoplay=true`}
       width="1920"
       height="1080"
     ></iframe>
